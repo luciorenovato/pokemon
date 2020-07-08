cb89afe62b5a47c63b8c28a5ecd91a9fbebda6bf
luciorenovato <github@renovato.com.br>
2020-07-08 01:20:55 -0300

    Commit inicial

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header reveal elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Pokemon Store
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="right = !right" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      right: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row justify-around" style="padding: 15px">
+      <div class="col-4" style="padding: 30px" v-for="creature in creatures" :key="creature.id">
+        <q-card class="my-card">
+          <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${creature.id}.png`">
+          <q-card-section>
+            <div class="text-h6">{{ creature.name }}</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none">
+            {{ lorem }}
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      creatures: [
+        { id: 1, name: 'Name 1' },
+        { id: 2, name: 'Name 2' },
+        { id: 3, name: 'Name 3' },
+        { id: 4, name: 'Name 4' },
+        { id: 5, name: 'Name 5' },
+        { id: 6, name: 'Name 6' }
+      ]
+    }
+  }
+}
+</script>
